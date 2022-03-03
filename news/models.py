882aef1bfe437e17b4a2a8c93187353bad309d66
Samuel Adewole <samadewole381@gmail.com>
2022-03-03 10:54:55 +0100

    added django-mptt

--- news/models.py
+++ news/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from mptt.models import MPTTModel, TreeForeignKey
+
 # Create your models here.
 from datetime import datetime
 
+
 class Story(models.Model):
-    #TODO set updated field as auto_now_add
+    # TODO set updated field as auto_now_add
     created = models.DateTimeField()
     updated = models.DateTimeField()
     score = models.IntegerField()
@@ -15,8 +18,6 @@ class Story(models.Model):
     type = models.CharField(max_length=200)
     time = models.CharField(max_length=200)
 
-
-
     def save(self, *args, **kwargs):
         if not self.id:
             self.timestamp = datetime.utcnow()
@@ -26,4 +27,52 @@ class Story(models.Model):
 class Comment(models.Model):
     hn_id = models.CharField()
     name = models.CharField()
-    news = models.ForeignKey(Story,related_name="children", on_delete=models.CASCADE)
+    news = models.ForeignKey(Story, related_name="children", on_delete=models.CASCADE)
+
+
+"""Comments of Comments are stored as Reply"""
+
+""""
+{
+  "by": "norvig",
+  "id": 2921983,
+  "kids": [
+    2922097,
+    2922429,
+    2924562,
+    2922709,
+    2922573,
+    2922140,
+    2922141
+  ],
+  "parent": 2921506,
+  "text": "Aw shucks, guys ... you make me blush with your compliments.<p>Tell you what, Ill make a deal: I'll keep writing if you keep reading. K?",
+  "time": 1314211127,
+  "type": "comment"
+}"""
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, related_name="replies", on_delete=models.CASCADE)
+    created = models. CharField(max_length=200)
+    hn_id = models.CharField(max_length=200)
+    time = models.CharField(max_length=200)
+    type = models.CharField(max_length=100)
+    text = models.CharField(max_length=2000)
+    hn_parent = models.CharField(max_length=200
+                                 )
+    kids = models.CharField(max_length=400)
+
+
+"""
+from myapp.models import Genre
+rock = Genre.objects.create(name="Rock")
+blues = Genre.objects.create(name="Blues")
+Genre.objects.create(name="Hard Rock", parent=rock)
+Genre.objects.create(name="Pop Rock", parent=rock)
+"""
+
+class Genre(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
+    comment = models.ForeignKey(Comment, related_name="comments", on_delete=models.CASCADE,blank=True,null=True)
+    class MPTTMeta:
+        order_insertion_by = ['name']
