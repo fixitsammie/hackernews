d0f735e584a0ecf66bf806c6c56474928f05a65c
Samuel Adewole <samadewole381@gmail.com>
2022-03-03 11:53:17 +0100

    Changed pagination page size to 100

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'hackernews.urls'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 100
 }
 
 TEMPLATES = [
